148cf470a92669e0b31043c25ee74e69e6d0956b
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-07-30 15:22:42 +0300

    Create program02.java

---
+++ 04/program02.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String args[]) {
+        double x, y, z;
+
+        x = 23.5;
+        System.out.println("x = " + x);
+        y = 7.3;
+        System.out.println("y = " + y);
+
+        z = x / y;
+        System.out.println("x / y = " + z);
+        z = x % y;
+        System.out.println("x % y = " + z);
+    }
+}
+
