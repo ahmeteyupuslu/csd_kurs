fd77326a60e42685c0ee3cd20122085444eadf1e
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-07-31 13:23:54 +0300

    Create iki basamaklı sayının basamaklarının karesini toplayan formul.java

---
+++ 05/iki basamaklı sayının basamaklarının karesini toplayan formul.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb = new java.util.Scanner(System.in);
+	    int a, b, c, n;
+	    
+	   	System.out.println("2 basamaklı bir sayı giriniz : ");
+	   	n = kb.nextInt();
+	   	
+	   	a = n / 10;     // onlar basamağı elde ediliyor
+	   	b = n % 10;     // birler basamağı elde ediliyor
+	   	
+	   	c = a * a + b * b;
+	   	
+	   	System.out.println ("Sonuç : " + c);	
+  }
+}
