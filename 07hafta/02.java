b6364a90e552a9126f4338b83f3b6bd0bb226b54
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-07 14:32:51 +0300

    Create 02.java
    ortanca değeri bulmak için program

---
+++ 07hafta/02.java
@@ -0,0 +1,32 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb = new java.util.Scanner(System.in);
+	    int a,b,c;
+	    
+	    System.out.print("A sayısını giriniz ");
+	    a = kb.nextInt();
+	    System.out.print("B sayısını giriniz ");
+	    b = kb.nextInt();
+	    System.out.print("C sayısını giriniz ");
+	    c = kb.nextInt();
+	    
+	    if (a>b)
+	        if (a>c)
+	            if(b>c)
+	                System.out.print("Ortanca Sayı B");
+	            else
+	                System.out.print("Ortanca sayı C");
+	       else
+	            System.out.print("Ortanca sayı A");
+	    else
+	        if (b>c)
+	            if (c>a)
+	                System.out.print("Ortanca sayı C");
+	            else
+	                System.out.print("Ortanca sayı A");
+	        else
+	            System.out.print("En büyük sayı B");
+	            
+	}
+}
