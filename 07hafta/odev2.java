304f1e73f459d3f51244693cc8e0f5664f7830ea
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-10 15:22:23 +0300

    Create odev2.java

---
+++ 07hafta/odev2.java
@@ -0,0 +1,9 @@
+//yirmiden küçük pozitif tek sayıları ekrana yazdıran program// 
+public class Main
+{
+	public static void main(String[] args) {
+	int i;
+	for (i=1; i<=20; i=i+2)	
+	System.out.println(i);
+	}
+}
