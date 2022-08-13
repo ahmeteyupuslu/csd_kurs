12d815ddba9a8d1565c29dc749954862b8d66adc
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-13 14:51:49 +0300

    Create 07.java

---
+++ 08/07.java
@@ -0,0 +1,22 @@
+// çözüm2 a ve b arasında 5 ve katlarını yazdırma
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb = new java.util.Scanner(System.in);
+		int a,i,b;
+		
+		System.out.print("a değerini giriniz : ");
+	    a = kb.nextInt();
+	    System.out.print("b değerini giriniz : ");
+	    b = kb.nextInt();
+	    
+	    for (i=a+(5-a%5); i<b ; i=i+5)
+	        System.out.println(i + " ");
+	    
+	 
+	}
+}
+	
+
