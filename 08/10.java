67a7359f76a51a52c85bab57c944cb43c9227a07
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-13 16:11:03 +0300

    Update 10.java

--- 08/10.java
+++ 08/10.java
@@ -1,1 +1,15 @@
-//
+//10dan 90a kadar 4er 4er artarak giden Fahrenheit cinsinden sıcaklık değerlerinin Celcious karşılıklarını ekrana yazdıran program.
+
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb = new java.util.Scanner(System.in);
+		double c,f;
+	 
+	    for (f=10; f<=90 ; f=f+4)
+	    {
+	        c=5.0/9.0*(f-32);
+	        System.out.println("C :" +c +" F: " +f);    }
+	}
+}
+
