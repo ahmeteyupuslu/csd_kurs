f3f806d01cb0fb21d9fcf1f6208b1ee368679318
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-13 13:15:04 +0300

    Create 01.java

---
+++ 08/01.java
@@ -0,0 +1,10 @@
+// ondan bire kadar olan sayıları ekrana yazdıran program
+
+public class Main
+{
+	public static void main(String[] args) {
+		int i;
+		for (i=10;i>=1; i=i-1)
+		    System.out.println(i);
+	}
+}
