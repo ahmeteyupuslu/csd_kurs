ed685af3dd58e54a660c44468a00a2df0aea27b0
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 13:32:48 +0300

    Create 03.java

---
+++ 09/03.java
@@ -0,0 +1,20 @@
+// for ile klavyeden girilen değerlerin toplanması.
+
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb=new java.util.Scanner(System.in);
+	    int i,n,sum;
+	    
+		sum = 0;
+		
+		for (i=1;i<=5;i=i+1) {
+		    System.out.print(i+". sayıyı giriniz : ");
+		    n = kb.nextInt();
+		    sum = sum + n;
+		}
+		
+	    System.out.println("Toplam : " +sum);
+		    
+	}
+}
