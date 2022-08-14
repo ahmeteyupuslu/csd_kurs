c23e9386f47dbc49e314321654a39e12b8324dec
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 14:54:02 +0300

    Create 06.java

---
+++ 09/06.java
@@ -0,0 +1,20 @@
+//dizideki tek sayıları yazdıran program
+
+public class Main
+    {
+    	public static void main(String[] args) {
+    	    
+    	    int a[]= {4,8,3,1,18,9,21,20,5,17};
+    	    int i, sum;
+    	    
+    	    
+    	
+    		for (i=0;i<10;i=i+1) 
+    		    if (a[i]%2!=0)
+    	      System.out.println(+a[i]);
+    	    
+    	   
+    	}
+    }
+
+
