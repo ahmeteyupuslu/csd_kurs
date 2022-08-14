be9e2ef4c37dff43e3f25a50ed093557c9282f47
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 15:21:59 +0300

    Update 07.java

--- 09/07.java
+++ 09/07.java
@@ -1,1 +1,24 @@
-//
+// dizideki tek sayıların adedini ekrana yazdıran program.
+
+public class Main
+    {
+    	public static void main(String[] args) {
+    	    
+    	    int a[]= {4,8,3,1,18,9,21,20,5,17};
+    	    int i, sum,size,count;
+    	    size=10;
+    	    
+    	    count=0;
+    	
+    		for (i=0;i<10;i=i+1) 
+    		    if (a[i]%2!=0){
+    		    System.out.println(a[i]);
+    		    count=count+1;
+    	      }
+    	        
+    	      System.out.println("Tek sayı adedi: "+counnt);
+    	    
+    	   
+    	}
+    }
+
