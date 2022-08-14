99af309a3e5f0f4a50a5e5f7d1243289a2644b52
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 15:50:14 +0300

    Update 09.java

--- 09/09.java
+++ 09/09.java
@@ -1,1 +1,22 @@
-// 
+// klavyeden x ve y giriliyor. x<y kabul. x'den büyük,y'den küçük dizi elemanlarını ekrana yazdıran program.
+public class Main
+{
+    public static void main(String[] args) 
+    {
+    java.util.Scanner kb = new java.util.Scanner(System.in);
+    int a[]= {4,8,3,1,18,9,21,20,5,17};
+    int i,size,x,y;	 
+    size = 10;
+    
+    System.out.print("x değerini giriniz : ");
+    x = kb.nextInt();
+    System.out.print("y değerini giriniz : ");
+    y = kb.nextInt();
+    
+    for (i = 0; i < size; i = i+1)
+        if (a[i]<y)
+        if (a[i]>x)
+        System.out.println(a[i]);
+    }
+}
+
