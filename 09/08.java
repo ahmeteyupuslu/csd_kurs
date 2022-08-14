13a9799dbe5a25282232460ce43ba9f5a6ad9c3c
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 15:31:33 +0300

    Create 08.java

---
+++ 09/08.java
@@ -0,0 +1,27 @@
+//serideki tek sayıları ve onların toplamını, ortalamasını, adetini yazdıran program.
+public class Main
+    {
+    	public static void main(String[] args) {
+    	    
+    	    int a[]= {4,8,3,1,18,9,21,20,5,17};
+    	    int i,size,count;	    
+    	    double sum,avg;
+    	   
+          size = 10;
+    	    count = 0;
+    	    sum = 0;
+    	
+    		for (i = 0; i < size; i = i+1) 
+    		    if (a[i] % 2 != 0){
+    		        System.out.println(a[i]);
+    		        sum=sum+a[i];
+    		        count=count+1;    }
+    	        
+    	  System.out.println("Tek sayı adedi: "+count);
+    	  System.out.println("Tek sayıların toplamı: " +sum);   
+        avg=sum/count;
+    	  System.out.println("Sayıların ortalaması: " +avg);
+    	        
+    	   
+    	}
+    }
