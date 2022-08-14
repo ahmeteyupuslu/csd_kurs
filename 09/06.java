c9a25e5f1be58edce28841f36a75e59e5d766e72
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-14 14:55:29 +0300

    Update 06.java

--- 09/06.java
+++ 09/06.java
@@ -5,7 +5,8 @@ public class Main
     	public static void main(String[] args) {
     	    
     	    int a[]= {4,8,3,1,18,9,21,20,5,17};
-    	    int i, sum;
+    	    int i, sum,size;
+            size = 10;
     	    
     	    
     	
