f8d698e4a2b1e2fb5968a2f353d38e43568b7364
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-21 16:13:42 +0300

    Update 02.java

--- 11/02.java
+++ 11/02.java
@@ -1,2 +1,21 @@
 //a dizisini kendi içinde tersine çeviren program
-  
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	int a[] = {4,8,3,1,18,9,21,20,5,17};
+	int i,size,temp,s;
+	size=10;
+	
+	
+	for(i=0; i<size/2; i=i+1)   {
+	    temp=a[i];
+	    a[i] = a[size-1-i];
+	    a[size-1-i]=temp;
+	}
+	
+		
+	for(i = 0; i<size;i=i+1)	
+		System.out.print(a[i]+" ");
+	}
+}
