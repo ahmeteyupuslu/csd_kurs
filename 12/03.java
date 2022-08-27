d6b41b4ae3f1389776776f9b64e6a3000c6bbe31
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-27 14:38:45 +0300

    Update 03.java

--- 12/03.java
+++ 12/03.java
@@ -1,1 +1,23 @@
 //a dizisinde tekleri başa,çiftleri sona atayan program.
+public class Main
+{
+    public static void main(String[] args){
+        int a[]={4,8,3,1,18,9,21,20,5,17};
+        int i,size,temp,p;
+        size=10;
+        p=0;
+        
+        for (i=0; i<size;i=i+1)
+            if (a[i]%2!=0)
+            {
+            temp=a[i];
+            a[i]=a[p];
+            a[p]=temp;
+            p=p+1;
+            }
+            
+        for (i=0;i<size;i=i+1)
+            System.out.print(a[i]+" ");
+     
+    }
+}
