93e0d61a1311a7a156a9cb24475451362e111566
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-27 15:14:39 +0300

    Create 06.java

---
+++ 12/06.java
@@ -0,0 +1,14 @@
+//dizinin en büyük elemanını ve indis numarasını bulan program.
+
+public class Main
+{
+    public static void main(String[] args){
+        int a[]={4,8,3,1,18,9,21,20,5,17};
+        int i,size,max;
+        size=10;
+        
+        max=a[0];
+          
+        for (i=1; i<size;i=i+1)
+            if (a[i]>max)
+                max=a[i];
