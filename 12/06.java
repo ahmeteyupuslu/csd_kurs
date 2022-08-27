2f1ed16fdff5df254edf1f131de5093bc048c268
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-27 15:29:27 +0300

    Update 06.java

--- 12/06.java
+++ 12/06.java
@@ -4,11 +4,19 @@ public class Main
 {
     public static void main(String[] args){
         int a[]={4,8,3,1,18,9,21,20,5,17};
-        int i,size,max;
+        int i,size,max,p;
         size=10;
         
         max=a[0];
+        p=0;
           
         for (i=1; i<size;i=i+1)
-            if (a[i]>max)
+            if (a[i]>max){  
                 max=a[i];
+                p=i;
+            }
+        
+        System.out.println("Dizinin en büyük elemanı : " + max);
+        System.out.println("İndis numarası:" + p);
+    }        
+}
