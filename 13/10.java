fd2d51fac7ded292eab6a0c8715ad739c75305b9
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-28 15:45:58 +0300

    Create 10.java

---
+++ 13/10.java
@@ -0,0 +1,20 @@
+//
+
+public class Main
+{
+	public static void main(String[] args) {
+    java.util.Scanner kb = new java.util.Scanner(System.in);
+    int n,i,k;
+    
+    System.out.print("n'yi giriniz : ");
+        n = kb.nextInt();
+    
+    for(i=1;i<=n;i=i+1){
+        for(k=1;k<=n-i;k=k+1)
+            System.out.print(" ");
+        for(k=1;k<=i;k=k+1)
+            System.out.print("*");
+        System.out.println();}
+        
+    }
+}
