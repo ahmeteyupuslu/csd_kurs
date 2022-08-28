78b08ae5bae3ada45926f2583042cc683289777e
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-28 14:23:22 +0300

    Create 04.java

---
+++ 13/04.java
@@ -0,0 +1,17 @@
+//dizinin her elemanını yanyaya 5 defa yazdıran program
+
+public class Main
+{
+	public static void main(String[] args) {
+    
+    int a[]={4,8,3,1,18,9,21,20,5,17};
+    int i,k,size;
+    size=10;
+    
+    for(i=0;i<size;i=i+1){
+        for(k=0;k<5;k=k+1)
+        System.out.print(a[i]+" ");
+        System.out.println();    
+        }
+    }
+}
