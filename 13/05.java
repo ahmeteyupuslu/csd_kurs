d2487e0de045789580a758ec2bedbd83d94c5f70
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-28 14:29:10 +0300

    Create 05.java

---
+++ 13/05.java
@@ -0,0 +1,17 @@
+//serideki her elemanı kendisi kadar yazdıran program.
+
+public class Main
+{
+	public static void main(String[] args) {
+    
+    int a[]={4,8,3,1,18,9,21,20,5,17};
+    int i,k,size;
+    size=10;
+    
+    for(i=0;i<size;i=i+1){
+        for(k=0;k<a[i];k=k+1)
+        System.out.print(a[i]+" ");
+        System.out.println();    
+        }
+    }
+}
