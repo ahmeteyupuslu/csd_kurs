338d1076def555e81e796428f17098d413a05e34
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-28 14:10:28 +0300

    Create 03.java

---
+++ 13/03.java
@@ -0,0 +1,17 @@
+//diziyi beş defa alt alta yazdıran program
+
+public class Main
+{
+	public static void main(String[] args) {
+    
+    int a[]={4,8,3,1,18,9,21,20,5,17};
+    int i,k,size,min,p,temp;
+    size=10;
+    
+    for(k=0;k<5;k=k+1){
+        for(i=0;i<size;i=i+1)
+        System.out.print(a[i]+" ");
+        System.out.println();    
+        }
+    }
+}
