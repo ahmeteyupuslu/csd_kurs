47a90e55203fa5fe7c2f5303c5abda6a4704b72b
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-08-28 14:53:25 +0300

    Create 08.java

---
+++ 13/08.java
@@ -0,0 +1,24 @@
+/*n 5 için 
+* 
+**
+***
+****
+*****    bu deseni veren program
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+    java.util.Scanner kb = new java.util.Scanner(System.in);
+    int n,i,k;
+    
+    System.out.print("n'yi giriniz : ");
+        n = kb.nextInt();
+    
+    for(i=1;i<=n;i=i+1){
+        for(k=1;k<=i;k=k+1)
+        System.out.print("*");
+        System.out.println();    
+        }
+    }
+}
