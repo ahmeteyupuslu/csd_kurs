d557ce7c1d71508f1979ffc83d406f4efa4503fb
ahmet eyup uslu <97919005+ahmeyus@users.noreply.github.com>
2022-09-11 15:05:55 +0300

    Update while.java

--- while.java
+++ while.java
@@ -8,8 +8,11 @@
    çevrim sayısının belli olduğu durumlarda for, olmadığı durumlarda while kullanılacak
  */
 
-public class Main {
 
+
+
+
+public class Main {
 	public static void main(String[] args) {
 		int i;
 
@@ -21,9 +24,12 @@ public class Main {
   	}
 }
 
-//klavyeden sürekli sayı giriliyor. 0 girildiğinde programdan çıkılıyor.
-public class Main {
 
+
+
+
+// klavyeden sürekli sayı giriliyor. 0 girildiğinde programdan çıkılıyor.
+public class Main {
 	public static void main(String[] args) {
 		java.util.Scanner kb = new java.util.Scanner(System.in);
     	int n;
@@ -35,212 +41,159 @@ public class Main {
   	}
 }
 
-//klavyeye girilen sayıları toplayan,1000'i geçtiğinde toplamı yazıdırıp programı sonlandıran kod.
+
+
+
+
+// klavyeye girilen sayıları toplayan,1000'i geçtiğinde toplamı yazıdırıp programı sonlandıran kod.
 public class Main {
+	public static void main(String[] args) {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+    	int n, z;
+    	z = 0;
 
-  public static void main(String[] args) {
-    java.util.Scanner kb = new java.util.Scanner(System.in);
-    int n, z;
-
-    z = 0;
-
-    while (z <= 1000) {
-      System.out.print("n'yi giriniz : ");
-      n = kb.nextInt();
-      z = z + n;
-    }
-    System.out.print("Toplam: " + z);
-  }
+    	while (z <= 1000) {
+    		System.out.print("n'yi giriniz : ");
+      		n = kb.nextInt();
+      		z = z + n;
+    	}
+    	System.out.print("Toplam: " + z);
+	}
 }
 
-//dizideki ilk tek sayıyı bulan program
-public class Main {
 
-  public static void main(String[] args) {
-    int a[] = { 4, 8, 3, 1, 18, 9, 21, 20, 5, 17 };
-    int i;
-    i = 0;
-    while (a[i] % 2 == 0) {
-      i = i + 1;
-    }
-    System.out.print("Serideki ilk tek sayı: " + a[i]);
-  }
+
+
+
+// dizideki ilk tek sayıyı bulan program
+public class Main {
+	public static void main(String[] args) {
+    	int a[] = { 4, 8, 3, 1, 18, 9, 21, 20, 5, 17 };
+    	int i;
+    	i = 0;
+    	while (a[i] % 2 == 0) {
+      	i = i + 1;
+    	}
+    	System.out.print("Serideki ilk tek sayı: " + a[i]);
+  	}
 }
 
-//klavyeden girilen sayıyı basamaklarına ayıran program.
+
+
+
+
+// klavyeden girilen sayıyı basamaklarına ayıran program.
 public class Main {
-
-  public static void main(String[] args) {
-    java.util.Scanner kb = new java.util.Scanner(System.in);
-    int n;
-
-    System.out.print("n'yi giriniz : ");
-    n = kb.nextInt();
-
-    while (n > 0) {
-      System.out.println(n % 10);
-      n = n / 10;
-    }
-  }
-}
-
-//klavyeden girilen sayıyı basamaklarına ayıran, sonrasında diziye kaydeden program.
-public class Main {
-
-  public static void main(String[] args) {
-    int a[] = new int[10];
-    java.util.Scanner kb = new java.util.Scanner(System.in);
-    int n, size, i, b;
-
-    System.out.print("n'yi giriniz : ");
-    n = kb.nextInt();
-
-    i = 0;
-    size = 0;
-
-    while (n != 0) {
-      a[i] = n % 10;
-      i = i + 1;
-      n = n / 10;
-      size = size + 1;
-    }
-    i = 0;
-    for (i = 0; i < size; i = i + 1) System.out.printf(a[i] + " ");
-  }
-}
-
-//klavyeden girilen sayının tersiyle yeni bir sayı elde eden program ve bunu 2 ile çarpan program.
-public class Main {
-
-  public static void main(String[] args) {
-    java.util.Scanner kb = new java.util.Scanner(System.in);
-    int n, size, k, a, m, c;
-
-    System.out.print("n'yi giriniz : ");
-    n = kb.nextInt();
-
-    k = 0;
-    c = 1;
-    m = n;
-
-    while (m != 0) {
-      m = m / 10;
-      c = c * 10;
-    }
-
-    while (n != 0) {
-      a = n % 10;
-      n = n / 10;
-      c = c / 10;
-      k = k + (a * c);
-    }
-    System.out.println("yeni sayı: " + k);
-    System.out.printf("yeni sayının iki katı: " + (2 * k));
-  }
-}
-
-//klavyeden girilen sayının basamaklarındaki sayılarla elde edilebilecek en büyük sayıyı bulan program.
-public class Main {
-
-  public static void main(String[] args) {
-    java.util.Scanner kb = new java.util.Scanner(System.in);
-    int n, k, i, p, temp, e;
-    int a[] = new int[10];
-
-    System.out.print("n'yi giriniz : ");
-    n = kb.nextInt();
-
-    i = 0;
-    p = 0;
-
-    while (n != 0) {
-      a[p] = n % 10;
-      n = n / 10;
-      p = p + 1;
-    }
-
-    for (i = 0; i < p - 1; i = i + 1) for (k = 0; k < p - 1 - i; k = k + 1) if (
-      a[k] < a[k + 1]
-    ) {
-      temp = a[k];
-      a[k] = a[k + 1];
-      a[k + 1] = temp;
-    }
-
-    e = 0;
-    for (i = 0; i < p; i = i + 1) {
-      e = e * 10 + a[i];
-    }
-    System.out.printf("Yeni sayı: " + e);
-  }
-}
-
-//dizideki ilk tek sayıyı ve devamındakileri 2 ile çarpan program.
-public class Main {
-
-  public static void main(String[] args) {
-    int a[] = { 4, 8, 3, 1, 18, 9, 21, 20, 5, 17 };
-    int i, size, flag;
-    size = 10;
-
-    flag = 0; //gelmedi
-
-    for (i = 0; i < size; i = i + 1) {
-      if (a[i] % 2 != 0) flag = 1;
-      if (flag == 1) a[i] = a[i] * 2;
-    }
-
-    for (i = 0; i < size; i = i + 1) System.out.print(a[i] + " ");
-  }
-}
-
-//klavyede girilen sayının dizide olup olmadığını ekrana yazdıran program.
-public class Main {
-
 	public static void main(String[] args) {
     	java.util.Scanner kb = new java.util.Scanner(System.in);
-    	int a[] = {5, 5, 3, 7, 8, 5, 7, 7, 7, 3, 8, 9, 8, 9, 8, 8, 15, 5, 8, 4};
-    	int i,n,size,flag;
-    	size=20;
+    	int n;
 
-    	flag=0;
-    
-    	System.out.print("n'yi giriniz:");
-    	n=kb.nextInt();
+    	System.out.print("n'yi giriniz : ");
+    	n = kb.nextInt();
 
-    	for (i=0;i<size;i=i+1){
-      		if (n==a[i])
-        		flag=1;}
+    	while (n > 0) {
+      	System.out.println(n % 10);
+      	n = n / 10;
+    	}
+	}
+}
 
-    	if (flag==0)
-        System.out.print (n + " sayısı seride yoktur.")
-        else 
-			System.out.print(n + " sayısı seride vardır.")
 
-    }
-  }
 
-  //klavyeden girilen indis nolu elemanın kendinden önce olup olmadığını ekrana yazdıran program.
- public class Main {
 
+
+// klavyeden girilen sayıyı basamaklarına ayıran, sonrasında diziye kaydeden program.
+public class Main {
 	public static void main(String[] args) {
+    	int a[] = new int[10];
     	java.util.Scanner kb = new java.util.Scanner(System.in);
-    	int a[] = {5, 5, 3, 7, 8, 5, 7, 7, 7, 3, 8, 9, 8, 9, 8, 8, 15, 5, 8, 4};
-    	int i,ind,size,flag;
-    	size=20;
+    	int n, size, i, b;
 
-    	flag=0;
-    	
-		System.out.print("indis numarasını giriniz:");
-    	ind=kb.nextInt();
+    	System.out.print("n'yi giriniz : ");
+    	n = kb.nextInt();
 
-		for (i=0;i<ind;i=i+1){
-			if (a[ind] == a[i])
-				flag=1;
-			}
-		if (flag == 0)
-			System.out.print("YOK");
-		else
-			System.out.print("VAR");
+    	i = 0;
+    	size = 0;
+
+    	while (n != 0) {
+      	a[i] = n % 10;
+      	i = i + 1;
+      	n = n / 10;
+      	size = size + 1;
+    	}
+    	i = 0;
+    	for (i = 0; i < size; i = i + 1) System.out.printf(a[i] + " ");
+  	}
+}
+
+
+
+
+
+// klavyeden girilen sayının tersiyle yeni bir sayı elde eden program ve bunu 2 ile çarpan program.
+public class Main {
+	public static void main (String[] args) {
+    	java.util.Scanner kb = new java.util.Scanner(System.in);
+    	int n, size, k, a, m, c;
+
+    	System.out.print("n'yi giriniz : ");
+    	n = kb.nextInt();
+
+    	k = 0;
+    	c = 1;
+    	m = n;
+
+    	while (m != 0) {
+      		m = m / 10;
+      		c = c * 10;
+    	}
+
+    	while (n != 0) {
+      		a = n % 10;
+      		n = n / 10;
+      		c = c / 10;
+      		k = k + (a * c);
+    	}
+    	System.out.println ("yeni sayı: " + k);
+    	System.out.printf ("yeni sayının iki katı: " + (2 * k));
+	}
+}
+
+
+
+
+
+// klavyeden girilen sayının basamaklarındaki sayılarla elde edilebilecek en büyük sayıyı bulan program.
+public class Main {
+	public static void main (String[] args) {
+    	java.util.Scanner kb = new java.util.Scanner(System.in);
+    	int n, k, i, p, temp, e;
+    	int a[] = new int[10];
+
+    	System.out.print("n'yi giriniz : ");
+    	n = kb.nextInt();
+
+    	i = 0;
+    	p = 0;
+
+    	while (n != 0) {
+      		a[p] = n % 10;
+      		n = n / 10;
+      		p = p + 1;
+    	}
+
+    	for (i = 0; i < p - 1; i = i + 1) 
+			for (k = 0; k < p - 1 - i; k = k + 1) 
+				if (a[k] < a[k + 1]) {
+      				temp = a[k];
+      				a[k] = a[k + 1];
+      				a[k + 1] = temp;
+   				}
+
+    	e = 0;
+    	for (i = 0; i < p; i = i + 1) {
+     	e = e * 10 + a[i];
+    	}	
+    	System.out.print ("Yeni sayı: " + e);
 	}
 }
